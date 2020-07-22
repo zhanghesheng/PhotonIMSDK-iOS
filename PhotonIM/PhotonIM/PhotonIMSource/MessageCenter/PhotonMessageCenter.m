4138cc81f4eaf6fb30590694f9713ab21e808457
zhangshuisheng <zhang.shuisheng@immomo.com>
2020-07-22 18:40:53 +0800

    trace模块crash，修复个别内存泄露

--- PhotonIM/PhotonIM/PhotonIMSource/MessageCenter/PhotonMessageCenter.m
+++ PhotonIM/PhotonIM/PhotonIMSource/MessageCenter/PhotonMessageCenter.m
@@ -197,12 +197,14 @@ static PhotonMessageCenter *center = nil;
 - (void)sendTex:(NSString *)text conversation:(nullable PhotonIMConversation *)conversation completion:(nullable CompletionBlock)completion{
     self.timeOut = 0;
     // 文本消息，直接构建文本消息对象发送
-    PhotonIMMessage *message = [PhotonIMMessage commonMessageWithFrid:[PhotonContent currentUser].userID toid:conversation.chatWith messageType:PhotonIMMessageTypeText chatType:conversation.chatType];
-    NSMutableArray *uids = [[NSMutableArray alloc] init];
-    [message setAtInfoWithAtType:PhotonIMAtTypeNoAt atList:uids];
-    PhotonIMTextBody *body = [[PhotonIMTextBody alloc] initWithText:text];
-    [message setMesageBody:body];
+    PhotonIMMessage *message = [PhotonIMMessage commonMessageWithFrid:[PhotonContent currentUser].userID toid:conversation.chatWith messageType:PhotonIMMessageTypeRaw chatType:conversation.chatType];
+//    NSMutableArray *uids = [[NSMutableArray alloc] init];
+//    [message setAtInfoWithAtType:PhotonIMAtTypeNoAt atList:uids];
+//    PhotonIMTextBody *body = [[PhotonIMTextBody alloc] initWithText:text];
+//    [message setMesageBody:body];
     
+    PhotonIMCustomBody *body = [PhotonIMCustomBody customBodyWithArg1:1 arg2:2 customData:[@"SDSDS" dataUsingEncoding:NSUTF8StringEncoding]];
+    [message setMesageBody:body];
     [self _sendMessage:message timeout:0 completion:completion];
     
 }
