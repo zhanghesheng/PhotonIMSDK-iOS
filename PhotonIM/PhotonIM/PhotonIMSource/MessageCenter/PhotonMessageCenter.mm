0665d0fa25e855b520a19cd874457af390dbaf6e
Bruce <zhang.shuisheng@immomo.com>
2020-11-18 13:43:30 +0800

    demo

--- PhotonIM/PhotonIM/PhotonIMSource/MessageCenter/PhotonMessageCenter.mm
+++ PhotonIM/PhotonIM/PhotonIMSource/MessageCenter/PhotonMessageCenter.mm
@@ -105,7 +105,7 @@ static PhotonMessageCenter *center = nil;
 }
 
 - (NSInteger)unreadMsgCount{
-    _unreadCount = [self.imClient getUnreadCountExcludeIgnore:@[]];
+    _unreadCount = [self.imClient getUnreadCountExcludeIgnore];
     return _unreadCount;
 }
 
